aede9026d0f13101835cab4da3eda9c83e50a85b
John Hitz <johnh@whodapet.com>
2021-01-25 14:04:39 -0600

    Init commit. Passing 1 test

---
+++ lib/rotational_cipher.ex
@@ -0,0 +1,21 @@
+defmodule RotationalCipher do
+  @doc """
+  Given a plaintext and amount to shift by, return a rotated string.
+
+  Example:
+  iex> RotationalCipher.rotate("Attack at dawn", 13)
+  "Nggnpx ng qnja"
+  """
+  @spec rotate(text :: String.t(), shift :: integer) :: String.t()
+  def rotate(text, shift) do
+    text
+    |> String.to_charlist()
+    |> Enum.reduce([], fn x, acc ->
+      [x + shift | acc]
+    end)
+    |> IO.inspect()
+    |> List.to_string()
+
+
+  end
+end
